166a3e22bd16ff28527715af7d07b97b67d2a1b8
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-08-18 16:24:18 +0200

    Refactor search movies logic and code

--- app.js
+++ app.js
@@ -1,18 +1,50 @@
 const APIKEY = "a268923f";
 
 const searchInput = document.querySelector(".search-input");
-const searchBtn = document.querySelector(".search-btn");
 const moviesContainer = document.querySelector(".movies-container");
 const watchIconContainer = document.querySelector(".watch-icon-container");
 const errorMessage = document.querySelector('.error-message');
 
-let moviesHTML = "";
+let moviesArr = [];
+let moviesCounter = 0;
 
-searchBtn.addEventListener("click", (e) => {
+
+
+// **************************************************************************************************************
+// |                                                Helper Functions                                            |
+// **************************************************************************************************************
+function hideElements(...elements) {
+  if(elements.length === 1) {
+    elements[0].style.display = 'none';
+  } else {
+    for(let element of elements) {
+      element.style.display = 'none';
+    }
+  }
+}
+
+function showElements(...elements) {
+
+  if(elements.length === 1) {
+    elements[0].style.display = 'block';
+  } else {
+    for(let element of elements) {
+      element.style.display = 'block';
+    }
+  }
+}
+
+
+// **************************************************************************************************************
+// |                                                Search Movies                                               |
+// **************************************************************************************************************
+
+// Upong Clicking On Search Button
+document.querySelector(".search-btn").addEventListener("click", (e) => {
   e.preventDefault();
 
-  // Clear Movies HTML Before Each Search
-  moviesHTML = "";
+  // Clear Movies Arr So that the new search replaces the old one
+  moviesArr = [];
 
   // Extract Search Input Value
   const searchValue = searchInput.value;
@@ -21,74 +53,216 @@ searchBtn.addEventListener("click", (e) => {
   if (searchValue) {
     getMovieId(searchValue);
   }
+
 });
 
+
+
 async function getMovieId(movieName) {
+  // Fetch All Movies That match The Provided Movie Name
   const response = await fetch(`http://www.omdbapi.com/?s=${movieName}&apikey=${APIKEY}`);
-  const movies = await response.json();
+  const {Search: movies, Response: responseMessage} = await response.json();
 
-  // Avoid Too many results error & Movie Not Found
-  if (movies.Response !== "False") {
-    // Hide the icon - Error -  and dispaly the movies container
-    watchIconContainer.style.display = "none";
-    moviesContainer.style.display = "block";
-    errorMessage.style.display = 'none';
 
+  // IF there are too many results or the movie is not found
+  if (responseMessage !== "False") {
+    // 1- Hide Watch Icon And Error Message 
+    hideElements(watchIconContainer, errorMessage);
+
+    // 2- Display Movie COntainer
+    showElements(moviesContainer);
+    
     // Only loop if there is more than 1 movie
-    if (movies.Search.length > 1) {
-      for (let movie of movies.Search) {
-        // call get movies based on movie id and give it the movie id
-        getMoviesBasedOnMovieId(movie.imdbID);
+    if (movies.length > 1) {
+      for (let movie of movies) {
+        // pass movie id and length of the movies 
+        getMovieDetailsBasedOnMovieId(movie.imdbID, movies.length);
       }
       // if there is only 1 movie , then extract that movie and do the same thing
-    } else if (movies.Search.length === 1) {
-      getMoviesBasedOnMovieId(movies.Search[0].imdbID);
+    } else if (movies.length === 1) {
+      getMovieDetailsBasedOnMovieId(movies[0].imdbID, movies.length);
     }
-  } else if(movies.Error) {
+  } else if(responseMessage === 'False') {
     // Hide Watch - movies continer And Show Error Message And Change Input Value
-    watchIconContainer.style.display = 'none';
-    moviesContainer.style.display = 'none';
-    errorMessage.style.display = 'block';
-    
+    // 1- Hide Watch Icon & Movies Container
+    hideElements(watchIconContainer, moviesContainer);
 
-    // Clear the input and update the placeholder
+    // 2-Show Error Message 
+    showElements(errorMessage);
+    
+    // 3- Clear The Input Value & Update The PlaceHolder
     searchInput.value = '';
     searchInput.placeholder = 'Searching something with no data';
-
   }
 }
 
-async function getMoviesBasedOnMovieId(movieId) {
-  const response = await fetch(
-    `http://www.omdbapi.com/?i=${movieId}&apikey=${APIKEY}`
-  );
+// Get Details Of The Movie Based On The Id
+async function getMovieDetailsBasedOnMovieId(movieId, movieLength) {
+  const response = await fetch(`http://www.omdbapi.com/?i=${movieId}&apikey=${APIKEY}`);
   const movie = await response.json();
 
-  // Build HTML out of this movie data
-  buildHTML(movie);
 
-  // !!!!!!!!!! Not efficient ! i'm manipulating the DOM more than once. !!!!!!!!!!!!!!!!!!!
-  moviesContainer.innerHTML = moviesHTML;
+  // Build An Object out of this movie data
+  buildMovieObj(movie);
+
+  // Store The HTML In A Varaible
+  const moviesHTML = buildMoviesHTML();
+
+
+  // Increase Movies Counter For Each Movie
+  moviesCounter++;
+
+  // When Movies Counter Is Equal To The Movies Number ( Fetch Is Over )
+  if(movieLength === moviesCounter) {
+      // Render The HTML ( To Manipulate The DOM Only Once For Better Performance ) & Reset The Counter
+      moviesContainer.innerHTML = moviesHTML;
+      moviesCounter = 0;
+  }
 }
 
-function buildHTML(movie) {
 
-    // If there is no poster - provide no poster image
-    let poster = movie.Poster !== 'N/A' ? movie.Poster : 'https://www.jakartaplayers.org/uploads/1/2/5/5/12551960/2297419_orig.jpg';
+function buildMovieObj(movie) {
+      // If there is no poster - provide a a default no poster image
+      let poster = movie.Poster !== 'N/A' ? movie.Poster : 'https://www.jakartaplayers.org/uploads/1/2/5/5/12551960/2297419_orig.jpg';
 
+      // Extract The Valuse Out Of Movie Object And Build An Object From It
+      const {imdbID, Title, imdbRating, Runtime, Genre,Plot} = movie;
+      const movieObj = {
+          imdbID,
+          poster,
+          Title,
+          imdbRating,
+          Runtime,
+          Genre,
+          Plot      
+     }
 
-  moviesHTML += `
-    <div class="movie">
-    <img src="${poster}" alt="${movie.Title}" class="movie-img">
-    <div class="movie-content">
-        <h3 class="movie-title">${movie.Title} <span class="movie-star">&starf;</span>  <span class="movie-rate">${movie.imdbRating}</span></h3>
-        <div class="movie-details">
-            <p class="mintues">${movie.Runtime}</p>
-            <p class="type">${movie.Genre}</p>
-            <button class="btn-watchlist"><i class="fa-solid fa-circle-plus plus-icon"></i> Watchlist</button>
-        </div>
-        <p class="movie-description">${movie.Plot}</p>
-    </div>
-    </div>
-    `;
+     // Push This Movie Object To An Array ( We will use this to build the html out of it)
+     moviesArr.push(movieObj);
 }
+
+
+function buildMoviesHTML() { 
+    const moviesHTML = moviesArr.map((singleMovie) => {
+      const {imdbID,poster,  Title, imdbRating, Runtime, Genre,Plot} = singleMovie;
+      return `
+      <div class="movie">
+      <img src="${poster}" alt="${Title}" class="movie-img">
+      <div class="movie-content">
+          <h3 class="movie-title">${Title} <span class="movie-star">&starf;</span>  <span class="movie-rate">${imdbRating}</span></h3>
+          <div class="movie-details">
+              <p class="mintues">${Runtime}</p>
+              <p class="type">${Genre}</p>
+              <button class="btn-watchlist"><i class="fa-solid fa-circle-plus plus-icon"></i> Watchlist</button>
+          </div>
+          <p class="movie-description">${Plot}</p>
+      </div>
+      </div>
+      `
+    }).join('')
+    return moviesHTML;
+}
+
+
+// **************************************************************************************************************
+// |                                                Watch List                                                  |
+// **************************************************************************************************************
+
+let watchlistArr;
+
+
+// If there is movies arr in the local storage then fetch it 
+if(localStorage.getItem('movies') ) {
+  watchlistArr = JSON.parse(localStorage.getItem('movies'))
+} else {
+  // Else initilaize an emtpy array
+  watchlistArr = [];
+}
+
+moviesContainer.addEventListener('click', (e) => {
+    const buttonWasClicked = e.target.classList.contains('btn-watchlist') || e.target.classList.contains('plus-icon')
+    if(buttonWasClicked ) {
+
+        // watchlistArr.push(e.target.parentElement.parentElement.parentElement)
+        // console.dir(e.target.parentElement.parentElement.parentElement)
+        const movieElement = e.target.parentElement.parentElement.parentElement.innerHTML;
+        let movieHTML = `<div class="movie">${movieElement}</div>`
+
+        movieHTML = movieHTML.replace(`<i class="fa-solid fa-circle-plus plus-icon"></i> Watchlist`, `<i class="fa-solid fa-circle-minus"></i> Remove`)
+        watchlistArr.push(movieHTML);
+        localStorage.setItem('movies', JSON.stringify(watchlistArr));
+
+
+
+
+
+      
+
+
+    }
+
+})
+
+
+const searchHeader = document.querySelector('.search-header');
+const searchForm = document.querySelector('.search');
+const watchlistHeader = document.querySelector('.watchlist-header');
+const goToWatchlist = document.querySelector('#go-to-watchlist');
+const goToSearchPage = document.querySelector('#go-to-search');
+const watchlistContent = document.querySelector('.watchlist-content');
+const watchlistContainer = document.querySelector('.watchlist-container')
+
+goToWatchlist.addEventListener('click', () => {
+  // Hide Search Header - Search Input & Watchlist Icon Container & Movies Container
+  searchHeader.style.display = searchForm.style.display = watchIconContainer.style.display = moviesContainer.style.display = 'none';
+
+
+  // Show Watchlist Header & Watchlist Content & Watchlist Container
+  watchlistHeader.style.display = watchlistContent.style.display = watchlistContainer.style.display =  'block';
+
+  let htmlMovie = '';
+
+
+  if(watchlistArr.length ) {
+    watchlistContent.style.display = 'none';
+
+
+
+
+    for(let movie of watchlistArr) {
+       htmlMovie += movie; 
+      
+    }
+    watchlistContainer.innerHTML = htmlMovie;
+  }
+
+
+})
+
+goToSearchPage.addEventListener('click', () => {
+    // Hide Watchlist Header & Watchlist Content & Watchlist Container
+    watchlistHeader.style.display = watchlistContent.style.display =  watchlistContainer.style.display = 'none';
+
+
+  // Show Search Header - Search Input & Watchlist Icon Container & Watchlist Container
+  searchHeader.style.display = searchForm.style.display = watchIconContainer.style.display = 'block';
+
+  if(moviesHTML !== '') {
+    // hide watch icon and show movies container
+    watchIconContainer.style.display = 'none';
+    moviesContainer.style.display = 'block';
+  }
+
+
+})
+
+
+watchlistContainer.addEventListener('click', (e) => {
+  const buttonWasClicked = e.target.classList.contains('btn-watchlist') || e.target.classList.contains('fa-circle-minus')
+  if(buttonWasClicked ) {
+    console.log('remove item now');
+    console.log(e.target.parentElement.parentElement.parentElement)
+    // console.log(JSON.parse(localStorage.getItem('movies')));
+  }
+
+})
